a198f665dd49093da1d7dbf55b9234e3e6a51617
AbdulRehman <likeam99@gmail.com>
2023-01-03 23:37:39 +0500

    objects

--- controller.js
+++ controller.js
@@ -1,14 +1,68 @@
-const button = document.querySelector('button');
+// const button = document.querySelector('button');
 
-button.addEventListener('click', txt);
+// button.addEventListener('click', txt);
 
-    function txt() {
-    const h1 = document.createElement('h1');
+//     function txt() {
+//     const h1 = document.createElement('h1');
 
-    h1.innerText = 'Abdul Rehamn';
-    document.body.prepend(h1);
+//     h1.innerText = 'Abdul Rehamn';
+//     document.body.prepend(h1);
 
-    setTimeout(()=>{
-        h1.remove();
-    },3000);
+//     setTimeout(()=>{
+//         h1.remove();
+//     },3000);
+// }
+
+
+// let odd = [];
+// let even = [];
+
+// function addMyFriends(friend) {
+//     myFriends.push(friend);
+//     console.log(myFriends);
+    
+// }
+
+// for(let i = 0; i <= 100; i++ ){
+
+//     if( i%2 == 0  ){
+//         even.push(i);
+//     }else{
+//         odd.push(i);
+//     }
+    
+// }
+
+
+// console.log(odd);
+// console.log(even);
+
+
+
+const khalifa1 = {
+    name: 'Abubakar',
+    age: 64
 }
+
+const khalifa2 = {
+    name: 'Umar',
+    age: 68
+}
+const khalifa3 = {
+    name: 'Usman',
+    age: 70
+}
+const khalifa4 = {
+    name: 'Ali',
+    age: 67
+}
+const khalifa5 = {
+    name: 'Mavia',
+    age: 72
+}
+
+function khulfaAge(khulfa){
+    console.log(khulfa.age)
+}
+
+khulfaAge(khalifa1);
